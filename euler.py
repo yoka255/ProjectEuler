5349b0672bf6e868cf788dc130319fdaf17a3f3c
yoka255 <katzyoav10@gmail.com>
2024-05-14 17:48:46 +0300

    Solve and add Fraction class

--- euler.py
+++ euler.py
@@ -2,7 +2,8 @@ import numpy as np
 from tqdm import tqdm
 import random
 from functools import reduce
-from typing import Tuple, List
+from typing import Tuple, List, Callable
+from math import gcd
 
 
 class SqrtNumber:
@@ -11,25 +12,92 @@ class SqrtNumber:
 		self.a = a
 		self.b = b
 		self.p = p
-	
 
 	def __add__(self, other: "SqrtNumber") -> "SqrtNumber":
 		assert self.p == other.p
 		return SqrtNumber(self.a + other.a, self.b + other.b, self.p)
 
-
 	def __mul__(self, other: "SqrtNumber") -> "SqrtNumber":
 		assert self.p == other.p
 		return SqrtNumber(self.a * other.a + self.b * other.b * self.p, self.a * other.b + self.b * other.a, self.p)
-	
 
 	def __sub__(self, other: "SqrtNumber") -> "SqrtNumber":
 		assert self.p == other.p
 		return SqrtNumber(self.a - other.a, self.b - other.b, self.p)
 
-
 	def __str__(self) -> str:
 		return f"{self.a} + {self.b} * sqrt({self.p})"
+
+
+class Fraction:
+
+	def __init__(self, a: int, b: int = 1):
+		if b == 0:
+			raise ZeroDivisionError("Tried to create Fraction object with denominator 0")
+		g = gcd(a, b)
+		self.a = a // g
+		self.b = b // g
+		if self.b < 0:
+			self.a *= -1
+			self.b *= -1
+
+	@staticmethod
+	def convert_to_fraction(x: "Fraction | int") -> "Fraction":
+		if isinstance(x, Fraction):
+			return x
+		if isinstance(x, int):
+			return Fraction(x)
+		raise NotImplementedError("Arithmetic operations between those types are not defined")
+
+	@staticmethod
+	def use_conversion(func: Callable[["Fraction", "Fraction"], "Fraction"]):
+		def wrapped(a: "Fraction", b: "Fraction | int") -> "Fraction":
+			return func(a, Fraction.convert_to_fraction(b))
+		return wrapped
+
+	@use_conversion
+	def __add__(self: "Fraction", other: "Fraction") -> "Fraction":
+		return Fraction(self.a * other.b + self.b * other.a, self.b * other.b)
+
+	@use_conversion
+	def __radd__(self: "Fraction", other: "Fraction") -> "Fraction":
+		return other + self
+
+	@use_conversion
+	def __mul__(self: "Fraction", other: "Fraction") -> "Fraction":
+		return Fraction(self.a * other.a, self.b * other.b)
+	
+	@use_conversion
+	def __rmul__(self: "Fraction", other: "Fraction") -> "Fraction":
+		return other * self
+	
+	@use_conversion
+	def __sub__(self: "Fraction", other: "Fraction") -> "Fraction":
+		return self + other * (-1)
+
+	@use_conversion
+	def __rsub__(self: "Fraction", other: "Fraction") -> "Fraction":
+		return other - self
+	
+	@use_conversion
+	def __truediv__(self: "Fraction", other: "Fraction") -> "Fraction":
+		return self * Fraction(other.b, other.a)
+
+	@use_conversion
+	def __rtruediv__(self: "Fraction", other: "Fraction") -> "Fraction":
+		return other / self
+
+	def __str__(self) -> str:
+		return f"{self.a} / {self.b}"
+
+	def __repr__(self) -> str:
+		return f"{self.a} / {self.b}"
+
+	def __eq__(self, other: "Fraction") -> bool:
+		return (self.a, self.b) == (other.a, other.b)
+
+	def __hash__(self) -> int:
+		return hash((self.a, self.b))
 
 
 def sieve(N: int) -> List[int]:
@@ -75,14 +143,6 @@ def digsum(n):
 	if n < 10:
 		return n
 	return n % 10 + digsum(n // 10)
-
-
-def gcd(a, b):
-	if b > a:
-		return gcd(b, a)
-	if b == 0:
-		return a
-	return gcd(b, a%b)
 
 
 def euclid(a : int, b : int) -> Tuple[int, int, int]:
